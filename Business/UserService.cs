f211343e37749d6274544cf278a43faa9468b28e
Payson Parker <paysonparker@costco.com>
2023-09-19 15:06:56 -0700

    Updates

--- Business/UserService.cs
+++ Business/UserService.cs
@@ -1,6 +1,14 @@
-﻿namespace Mulligan.API.Business
+﻿using Mulligan.API.Data;
+
+namespace Mulligan.API.Business
 {
     public class UserService
     {
+        private readonly MulliganDbContext _dbContext;
+
+        public UserService(MulliganDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
     }
 }
